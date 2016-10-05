a6ae7cdad84af3a61ba67b5257d0d1ce2684328f
Bmar21 <brandonmalor21@gmail.com>
2016-10-05 12:13:09 -0600

    Linked the added ViewController to the appropriate .swift file

---
+++ Transitions/UpdatedViewContoller.swift
@@ -0,0 +1,19 @@
+//
+//  UpdatedViewContoller.swift
+//  Transitions
+//
+//  Created by Marlor, Brandon on 10/5/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class UpdatedViewContoller : UIViewController
+{
+    override func viewDidLoad()
+    {
+        //Load the parent version of a View.
+        super.viewDidLoad()
+        //Load this pages components
+    }
+}
